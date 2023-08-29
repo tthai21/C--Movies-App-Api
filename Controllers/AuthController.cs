98f9a7ee829b17675beb13d0937494c4eb404bcb
Alex Thai <tthai21@student.holmesglen.edu.au>
2023-08-30 01:46:00 +1000

    Create JWT

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace C__Movies_App_Api.Controllers;
 
@@ -9,6 +13,12 @@ namespace C__Movies_App_Api.Controllers;
 public class AuthController : ControllerBase
 {
     public static User user = new User();
+    private IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
     [HttpPost("register")]
     public async Task<ActionResult<User>> Register(UserDto request)
@@ -32,9 +42,38 @@ public class AuthController : ControllerBase
         {
             return BadRequest("Email not found");
         }
-        return Ok("My Token");
+        if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+        {
+            return BadRequest("Password Invalid");
+        }
+
+        string token = CreateToken(user);
+
+        return Ok(token);
     }
 
+    private string CreateToken(User user)
+    {
+
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: cred
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
@@ -50,7 +89,7 @@ public class AuthController : ControllerBase
         using (var hmac = new HMACSHA512(passwordSalt))
         {
             var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            return computedHash.SequenceEqual(passwordHash); 
+            return computedHash.SequenceEqual(passwordHash);
 
         }
     }
